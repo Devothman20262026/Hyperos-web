244d50487dfbd18c3c2c682c97740208eeb845bc
Devothman20262026 <othmanalif11@gmail.com>
2026-01-30 23:05:48 +0100

    Delete LoxkScreen.tsx

--- LoxkScreen.tsx
+++
@@ -1,8 +0,0 @@
-export default function LockScreen({ onUnlock }: { onUnlock: () => void }) {
-  return (
-    <div className="lock" onClick={onUnlock}>
-      <div className="time">{new Date().toLocaleTimeString([], {hour:'2-digit',minute:'2-digit'})}</div>
-      <div className="hint">Tap to unlock</div>
-    </div>
-  )
-}
